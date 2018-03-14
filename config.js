49dedd9cf607b0dae5a34c167c377227fc80398b
Keenan Nicholson <knicholson32@gatech.edu>
2018-03-14 00:41:02 -0400

    Formatting, added Dev branch, starting CI testing. Issues #7 and #8.

--- config.js
+++ config.js
@@ -6,14 +6,14 @@
 // variable below.
 // ========================================================== //
 let system = {
-  port: 8081,           // Port to bind websocket server
-  max_clients: 2,       // Max number of allowed clients
-  key: 'pa88w0r4',      // Default interface key
+  port: 8081, // Port to bind websocket server
+  max_clients: 2, // Max number of allowed clients
+  key: 'pa88w0r4', // Default interface key
   directory: './files', // Storage location
-  log: './.log',        // Log location
-  ssl: false,           // Use SSL to encrypt websocket
-  cert: './certificate.pem',  // Path to SSL certificate
-  key: './key.pem'            // Path to SSL key
+  log: './.log', // Log location
+  ssl: false, // Use SSL to encrypt websocket
+  cert: './certificate.pem', // Path to SSL certificate
+  key: './key.pem' // Path to SSL key
 };
 // ========================================================== //
 
@@ -22,9 +22,9 @@ var md5 = require('md5');
 
 // Process user input key
 // Loop through file args
-process.argv.forEach(function (val, index, array) {
+process.argv.forEach(function(val, index, array) {
   // If there is a key set command, keep note of the next index
-  if((val == "-key" || val == "-k" || val == "key") && index + 1 < process.argv.length){
+  if ((val == "-key" || val == "-k" || val == "key") && index + 1 < process.argv.length) {
     // Set the new key and return
     system.key = process.argv[index + 1];
     return;
@@ -32,7 +32,7 @@ process.argv.forEach(function (val, index, array) {
 });
 
 // General function for generating a unique id
-function getID () {
+function getID() {
   return Math.random().toString(36).substr(2, 9);
 };
 let session_id = getID();
