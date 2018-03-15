3841de33df2c78ada136488b86b30a636eb6a38d
Keenan Nicholson <knicholson32@gatech.edu>
2018-03-15 16:02:01 -0400

    Updated `main.js` and the README.md to align with the Object Based operation.

--- test/host_test_server.js
+++ test/host_test_server.js
@@ -23,7 +23,6 @@ let users = [{
 let ms = new MicroServer({
   port: 8081, // Port to bind websocket server
   max_clients: 1, // Max number of allowed clients
-  key: 'test_key', // Default interface key
   directory: './files', // Storage location
   log: './.log', // Log location
   ssl: false, // Use SSL to encrypt websocket
